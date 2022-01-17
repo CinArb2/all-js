9e149fabdee5c2fcf3ef655e567ef8de18a539d3
CinArb2 <91969387+CinArb2@users.noreply.github.com>
2022-01-17 11:42:57 -0500

    exercises 01/17

--- all-js.js
+++ all-js.js
@@ -11,3 +11,28 @@ function findRepeatedAge(students) {
     
   return Number(Object.keys(objetoContar).reduce((a, b) => objetoContar[a] > objetoContar[b] ? a : b));
 }
+
+//================================
+
+function countStudents(students, countries, countryName) {
+    return students.map(el=>{
+        for(let parametros of countries){
+            if (el['country_id'] === parametros['id']) return parametros['name'];
+        }
+    }).reduce((contador, item)=> (item === countryName)? contador + 1 : contador, 0)
+}
+
+//==================================
+
+function sumEvens(start, end) {
+    let contador = 0;
+    let ini = start
+    if(start % 2 !== 0){
+        ini = start - 1
+    }
+    for ( let i = ini + 2 ; i < end; i+=2){
+        contador += i;
+    }
+    return contador;
+}
+
